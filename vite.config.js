ac14f35bc4629ac0614dd697d8e3576ca6e28f8e
Rahul9099 <78953369+Rahul9099@users.noreply.github.com>
2023-07-08 00:22:34 +0530

    done with api rendering data next will be pagination

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
 import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    // vite config
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
+// export default defineConfig(({ command, mode }) => {
+//   // Load env file based on `mode` in the current working directory.
+//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+//   const env = loadEnv(mode, process.cwd(), '')
+//   return {
+//     // vite config
+    
+//     define: {
+//       __APP_ENV__: JSON.stringify(env.APP_ENV),
+//     },
+//     root:"src",
+//   }
+// })
+
+export default {
+  // Other Vite options...
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Specify the correct path to your entry point file
     },
-    root:"src",
-  }
-})
+    optimizeDeps: {
+      // include: ['src/**/*.js'], // Add file patterns to include for pre-bundling
+      include: ['src/*'],
+    },
+  },
+};
+
